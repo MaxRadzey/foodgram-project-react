7a171e7e39a4f30f65d218096a90206e8b464bcd
Максим Радзей <max.radzey@yandex.ru>
2024-04-09 19:25:47 +0300

    Исправил настройки проекта

--- foodgram_backend/foodgram_backend/settings.py
+++ foodgram_backend/foodgram_backend/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,12 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f+vb5@yv(-n_3v)q3)t^+-e#i&$z(h&(z5k$#x--9%m($8hcf_'
+# SECRET_KEY = 'django-insecure-f+vb5@yv(-n_3v)q3)t^+-e#i&$z(h&(z5k$#x--9%m($8hcf_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['84.252.139.18', 'localhost', '127.0.0.1', 'maxmaxrr.ru']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
 
 
 # Application definition
@@ -121,9 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
