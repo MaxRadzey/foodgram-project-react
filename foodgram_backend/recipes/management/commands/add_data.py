5d1313315e4f78388b9c0369aeb3271cb7b4c6e2
Максим Радзей <max.radzey@yandex.ru>
2024-04-09 20:00:28 +0300

    Добавил добавление данных в БД

---
+++ foodgram_backend/recipes/management/commands/add_data.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = ('Импорт данных из csv файла в БД.'
+            'Перед импортом данных необходимо удалить БД.')
+
+    def handle(self, *args, **options):
+        self.stdout.write('Добавление базы данных!')
+        relative_path = "data"
+        absolute_path = os.path.abspath(relative_path)  # Путь к папке с файлам
+
+        CSV_DATA_AND_MODELS = (
+            ('ingredients.csv', Ingredient),
+        )
+
+        for file_name, models in CSV_DATA_AND_MODELS:
+            with open(
+                f'{absolute_path}/{file_name}', 'r', encoding='utf-8'
+            ) as csvfile:
+                data = csv.DictReader(csvfile)
+                list_to_add_in_db = []
+                for row in data:
+                    list_to_add_in_db.append(models(**row))
+                models.objects.bulk_create(list_to_add_in_db)
+        self.stdout.write('База данных добавлена!')
