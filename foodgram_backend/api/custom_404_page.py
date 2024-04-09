53f157431fa82ce3f372f4ad2929a694e49e5acd
Максим Радзей <max.radzey@yandex.ru>
2024-04-09 22:45:13 +0300

    f ver

--- foodgram_backend/api/custom_404_page.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-
-def custom404(request, exception=None):
-    return JsonResponse({
-        'status_code': 404,
-        'error': 'Ошибка 404 - страница не найдена!'
-    })
