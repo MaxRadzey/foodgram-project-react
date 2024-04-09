4bee7010f84f4e6bce10a32fcd61cacbb2303296
Максим Радзей <max.radzey@yandex.ru>
2024-04-09 20:32:22 +0300

    Исправил ошибку в настройках проекта

--- foodgram_backend/foodgram_backend/settings.py
+++ foodgram_backend/foodgram_backend/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-f+vb5@yv(-n_3v)q3)t^+-e#i&$z(h&(z5k$#x--9%m($8hcf_'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
